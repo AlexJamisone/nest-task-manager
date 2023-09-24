91d967050fc9daa0bebb357a4be6990c4c286d5c
AlexJamisone <business.homeit@gmail.com>
2023-09-24 22:15:23 +0300

    Make delet and updateStatus methood for db

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,19 +1,26 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import {
+	Patch,
+	Delete,
+	Controller,
+	Get,
+	Param,
+	Post,
+	Body,
+	Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
 	constructor(private tasksService: TasksService) {}
-	// @Get()
-	// getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-	// 	if (Object.keys(filterDto).length) {
-	// 		return this.tasksService.getTasksWithFilters(filterDto);
-	// 	} else {
-	// 		return this.tasksService.getAllTasks();
-	// 	}
-	// }
+	@Get()
+	getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
+		return this.tasksService.getTask(filterDto);
+	}
 
 	@Get('/:id')
 	getTaskById(@Param('id') id: string): Promise<Task> {
@@ -27,17 +34,17 @@ export class TasksController {
 	// update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
 	// 	return this.tasksService.update(updateTaskDto, id);
 	// }
-	// @Patch('/:id/status')
-	// updateStatus(
-	// 	@Param('id') id: string,
-	// 	@Body() updateTaskStatus: UpdateTaskStatusDto,
-	// ): Task {
-	// 	const { status } = updateTaskStatus;
-	// 	return this.tasksService.updateStatus(id, status);
-	// }
+	@Patch('/:id/status')
+	updateStatus(
+		@Param('id') id: string,
+		@Body() updateTaskStatus: UpdateTaskStatusDto,
+	): Promise<Task> {
+		const { status } = updateTaskStatus;
+		return this.tasksService.updateTaskStatus(id, status);
+	}
 
-	// @Delete('/:id')
-	// deletTask(@Param('id') id: string): void {
-	// 	this.tasksService.deletTask(id);
-	// }
+	@Delete('/:id')
+	deletTask(@Param('id') id: string): void {
+		this.tasksService.deletTask(id);
+	}
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,8 @@ import { TaskRepository } from './task.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,6 +12,11 @@ export class TasksService {
 		@InjectRepository(TaskRepository)
 		private tasksRepository: TaskRepository,
 	) {}
+
+	getTask(filterDto: GetTasksFilterDto): Promise<Task[]> {
+		
+	}
+
 	createTask(createTaskDto: CreateTaskDto): Promise<Task> {
 		return this.tasksRepository.createTask(createTaskDto);
 	}
@@ -23,5 +30,19 @@ export class TasksService {
 			throw new NotFoundException(`Task with ID: ${id} not found`);
 		}
 		return tasks;
+	}
+
+	async deletTask(id: string): Promise<void> {
+		const result = await this.tasksRepository.delete(id);
+		if (result.affected === 0) {
+			throw new NotFoundException(`Task with ${id} not found`);
+		}
+	}
+
+	async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+		const task = await this.getById(id);
+		task.status = status;
+		await this.tasksRepository.save(task);
+		return task;
 	}
 }
