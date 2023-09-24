1aa0e45c8b9221f6cdc1fac0975205be5db5c444
AlexJamisone <business.homeit@gmail.com>
2023-09-24 11:21:59 +0300

    make db get task id func

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,5 @@
 import { IsEnum, IsOptional, IsString } from 'class-validator';
-import { TaskStatus } from '../task.model';
+import { TaskStatus } from '../task-status.enum';
 
 export class GetTasksFilterDto {
 	@IsOptional()
@@ -9,6 +9,4 @@ export class GetTasksFilterDto {
 	@IsOptional()
 	@IsString()
 	search?: string;
-}  
-
-
+}
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,7 @@
-import { TaskStatus } from "../task.model";
+import { TaskStatus } from '../task-status.enum';
 
 export class UpdateTaskDto {
 	title: string;
 	description: string;
-	status: TaskStatus
+	status: TaskStatus;
 }
