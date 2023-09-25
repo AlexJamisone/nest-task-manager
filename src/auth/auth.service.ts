5f621e83d58f78d8401feb3ab6c1d753c5cdd30a
AlexJamisone <business.homeit@gmail.com>
2023-09-25 18:52:05 +0300

    Add bcrypt to store passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
 	constructor(
@@ -11,5 +11,20 @@ export class AuthService {
 	) {}
 	signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
 		return this.userRepository.createUser(authCredentialsDto);
+	}
+	async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+		const { password, username } = authCredentialsDto;
+		const user = await this.userRepository.findOne({
+			where: {
+				username,
+			},
+		});
+		if (user && (await bcrypt.compare(password, user.password))) {
+			return 'success';
+		} else {
+			throw new UnauthorizedException(
+				'Please check you login credentials',
+			);
+		}
 	}
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,12 @@
 import { DataSource, Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import {
+	Injectable,
+	ConflictException,
+	InternalServerErrorException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -10,10 +15,23 @@ export class UserRepository extends Repository<User> {
 	}
 	async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
 		const { password, username } = authCredentialsDto;
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(password, salt);
 		const user = this.create({
 			username,
-			password,
+			password: hashedPassword,
 		});
-		await this.save(user);
+
+		try {
+			await this.save(user);
+		} catch (error) {
+			//duplicate username or server error
+			console.log(typeof error.code);
+			if (error.code === '23505') {
+				throw new ConflictException('User already exitsts');
+			} else {
+				throw new InternalServerErrorException();
+			}
+		}
 	}
 }
